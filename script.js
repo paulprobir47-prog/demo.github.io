6bd54b4d9c3262aa5de15c63bb944a4844581796
Propaul <paulprobir47@gmail.com>
2026-07-01 00:32:19 +0530

    Implement theme toggle feature with localStorage
    Added theme toggle functionality and saved theme preference.

--- script.js
+++ script.js
@@ -2,3 +2,25 @@ console.log("Pulse Medicare Dashboard Loaded");
 window.location.href="dashboard.html";
 if(username=="admin" && password=="12345")
 window.location.href = "dashboard.html";
+<script>
+const body = document.body;
+const btn = document.getElementById("themeBtn");
+
+// Load saved theme
+if (localStorage.getItem("theme") === "dark") {
+    body.classList.add("dark-mode");
+    btn.innerHTML = "☀️ Light Mode";
+}
+
+function toggleTheme() {
+    body.classList.toggle("dark-mode");
+
+    if (body.classList.contains("dark-mode")) {
+        localStorage.setItem("theme", "dark");
+        btn.innerHTML = "☀️ Light Mode";
+    } else {
+        localStorage.setItem("theme", "light");
+        btn.innerHTML = "🌙 Dark Mode";
+    }
+}
+</script>
